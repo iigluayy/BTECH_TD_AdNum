67d01f57c0b7d887ed2fac1dc411aa73ef56b5f3
masyan455 <109448816+masyan455@users.noreply.github.com>
2022-08-20 19:42:42 +0700

    Add-btOK

--- Dayweek_TD/Dayweek_TD/Form1.vb
+++ Dayweek_TD/Dayweek_TD/Form1.vb
@@ -1,5 +1,25 @@
-﻿Public Class Dayweek
+﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub btOK_Click(sender As Object, e As EventArgs) Handles btOK.Click
+        Dim num As Integer
+        If 'เช็คค่าว่าง
+        Else
+            If 'เช็คค่าตัวเลข
+                If 'เช็คเลข 1-7
+                Else
+
+                    Select Case num
+                        'เงื่อนไข case
+
+                    End Select
+                End If 'Check Dayweek
+            Else
+                'เช็คค่าตัวเลข
+            End If 'End Check numeric data
+
+        End If 'Check End blank data
     End Sub
 End Class
